15fb7e0c08fb64b14f15e7a07afc844b62736f51
Park Tae Hyeong <tae020117@gmail.com>
2019-07-20 17:47:08 +0900

    [FIX] web.php router
    고쳤긴한데 테스트는 아직 안해봄 오류날수도

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
             $router->get('/', 'UserController@index');
             $router->post('/', 'UserController@create');
             $router->get('/change','UserController@showChange');
-            $router->get('/test', 'UserController@search');
+            $router->get('/search', 'UserController@search');
             $router->get('/count', 'UserController@count');
             $router->get('/{id}', 'UserController@show');
             $router->patch('/{id}', 'UserController@update');
@@ -20,6 +20,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         $router->group(['prefix' => 'messages'], function () use ($router) {
             $router->get('/', 'MessageController@index');
             $router->post('/', 'MessageController@create');
+            $router->post('/send', 'MessageController@send');
             $router->get('/count', 'MessageController@count');
             $router->get('/{id}', 'MessageController@show');
             $router->patch('/{id}', 'MessageController@update');
