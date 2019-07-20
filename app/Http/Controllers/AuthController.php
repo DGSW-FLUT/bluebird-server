a1e8e9229bf645023fd422ff8704927e3e94222e
Park Tae Hyeong <tae020117@gmail.com>
2019-07-20 16:32:22 +0900

    [FIX] jwt token 유효기간 60일로
    나중에 수정 필요

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends BaseController
             'iss' => "lumen-jwt", // Issuer of the token
             'sub' => $user->id, // Subject of the token
             'iat' => time(), // Time when JWT was issued. 
-            'exp' => time() + 60*60 // Expiration time
+            'exp' => time() + 60*60*24*60 // Expiration time
         ];
         
         // As you can see we are passing `JWT_SECRET` as the second parameter that will 
