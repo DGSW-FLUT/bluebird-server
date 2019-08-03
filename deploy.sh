2e2a20c46c55a0233cb245f0ff294234cc19eee4
서상희 <tbvjaos510@naver.com>
2019-08-03 18:19:36 +0900

    [ADD] add deploy.sh to client

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,11 @@
 
 # update source code
 git pull origin master --force
+git -C ../bluebird-client pull --force
+
+rm -rf resources/client
+cp ../bluebird-client/src/ resources/client -r
+yarn
+yarn build
 
 # update composer
