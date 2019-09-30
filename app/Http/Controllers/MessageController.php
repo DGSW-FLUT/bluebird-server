beb802ee705b7c0116b3703fbcb7d5f8e296ca87
J1NC <womin1120@naver.com>
2019-09-30 10:53:06 +0900

    [MODIFY] 임시 출력

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -143,6 +143,7 @@ class MessageController extends Controller
             return response()->json(["status" => 500, "message" => '[CODE]'.$resultCode.' FILE UPLOAD ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
+        print_r($json_decode($result, true));
         $fileId = json_decode($result, true)["file"]["fileId"];
 
         $requestNum = str_replace("-","",$input['request_num']);
